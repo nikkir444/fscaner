46f78ac7a54f258634f51599bd0173a1014ebbf0
valdemar102 <valdemar102@gmail.com>
2019-04-15 21:42:54 +0300

    create project

---
+++ src/main/java/com/aproach2/fscaner/FscanerApplication.java
@@ -0,0 +1,13 @@
+package com.aproach2.fscaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FscanerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FscanerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aproach2/fscaner/FscanerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aproach2.fscaner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FscanerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
