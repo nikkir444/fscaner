cd159ea064cdbc56634677addd540d7be8a74d84
valdemar102 <valdemar102@gmail.com>
2019-04-18 19:42:38 +0300

    Table component added

--- web/fscaner-app/src/app/app.module.ts
+++ web/fscaner-app/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng2SmartTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
